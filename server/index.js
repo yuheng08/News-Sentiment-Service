df0669cc0fc3fd5e9b52bc3786e4e6131a044e80
Yuheng Huang <yuheng08@gmail.com>
2017-11-02 17:34:35 -0700

    implemented some basic clustering

--- server/index.js
+++ server/index.js
@@ -1,18 +1,37 @@
+const cluster = require('cluster');
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(morgan('tiny'));
-app.use(bodyParser.json());
+if (cluster.isMaster) {
+  const numCPUs = require('os').cpus().length;
 
-app.post('/tweets', function (req, res) {
-  console.log(req.body);
-  res.end();
-});
+  console.log(`Master ${process.pid} is running`);
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-app.listen(PORT, function () {
-  console.log(`App is listening on port ${PORT}!`);
-});
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  const app = express();
+
+  app.use(morgan('tiny'));
+  app.use(bodyParser.json());
+
+  console.log(`Worker ${process.pid} started`);
+  app.post('/tweets', function (req, res) {
+    console.log(req.body);
+    console.log(`Process ${process.pid} is working...`);
+    res.end();
+  });
+  
+  app.listen(PORT, function () {
+    console.log(`${process.pid} is listening on port ${PORT}!`);
+  });
+}
+
